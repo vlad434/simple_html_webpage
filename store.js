4d9ff3a3b12968060f9c5e72e1556a7abf690b0f
unknown <vladilie06@yahoo.com>
2021-01-28 12:17:44 +0200

    second

--- store.js
+++ store.js
@@ -1,32 +1,131 @@
-var removeCartItemButtons = document.getElementsByClassName('btn-danger')
- 
-for (var i=0; i < removeCartItemButtons.length; i++){
-    var button = removeCartItemButtons[i];
-    button.addEventListener('click', function(event){    //adauga un event pe buton si ii spune sa faca ceva
-       var buttonClicked =  event.target;
-       buttonClicked.parentElement.parentElement.remove();  //de ce "parentElement si nu numele clasei parinte???"
-        updateCartTotal();
-    })
- 
+if (document.readyState == "loading") {
+  document.addEventListener(" DOMContentLoaded", ready);
+} else {
+  ready();
 }
 
-function updateCartTotal(){
-    var cartItemContainer = document.getElementsByClassName('cart-items')[0];
-    var cartRows = cartItemContainer.getElementsByClassName('cart-row');
-    var total = 0;
-    for (var i=0; i < cartRows.length; i++){
-        var cartRow = cartRows[i];
-        var priceElement = cartRow.getElementsByClassName('cart-price')[0];
-        var quantityElement = cartRow.getElementsByClassName('cart-quantity-input')[0];
-        
-        var price = parseFloat(priceElement.innerText.replace('$', '')); 
-        //innerText retine valoarea afisata in html pentru pret
-        //parseFloat transf un string intr-un float, adica nr
-        var quantity = quantityElement.value;
+function ready() {
+  var removeCartItemButtons = document.getElementsByClassName("btn-danger");
 
-        total = total + ( price * quantity);
+  for (var i = 0; i < removeCartItemButtons.length; i++) {
+    var button = removeCartItemButtons[i];
+    button.addEventListener("click", removeCartItem);
+    //adauga un event pe buton si ii spune sa faca ceva
+  }
+
+  var quantityInputs = document.getElementsByClassName("cart-quantity-input");
+  for (var i = 0; i < removeCartItemButtons.length; i++) {
+    var input = quantityInputs[i];
+    input.addEventListener("change", quantityChanged);
+  }
+
+  var addToCartButtons = document.getElementsByClassName("shop-item-button");
+  for (var i = 0; i < addToCartButtons.length; i++) {
+    var button = addToCartButtons[i];
+    button.addEventListener("click", addToCartClicked);
+  }
+
+  document
+    .getElementsByClassName("btn-purchase")[0]
+    .addEventListener("click", purchaseClicked);
+}
+
+function purchaseClicked() {
+  alert("Thank u for your purchase");
+  var cartItems = document.getElementsByClassName("cart-items")[0];
+  while (cartItems.hasChildNodes()) {
+    cartItems.removeChild(cartItems.firstChild);
+  }
+
+  updateCartTotal();
+}
+
+function removeCartItem(event) {
+  var buttonClicked = event.target;
+  buttonClicked.parentElement.parentElement.remove();
+  updateCartTotal();
+}
+
+function quantityChanged(event) {
+  var input = event.target;
+  if (isNaN(input.value) || input.value <= 0) {
+    input.value = 1;
+  }
+  updateCartTotal();
+}
+
+function addToCartClicked(event) {
+  var button = event.target;
+  var shopItem = button.parentElement.parentElement;
+  var title = shopItem.getElementsByClassName("shop-item-title")[0].innerText;
+  var price = shopItem.getElementsByClassName("shop-item-price")[0].innerText;
+  var imageSrc = shopItem.getElementsByClassName("shop-item-image")[0].src;
+  console.log(title, price, imageSrc);
+  addItemToCart(title, price, imageSrc);
+  updateCartTotal();
+}
+
+function addItemToCart(title, price, imageSrc) {
+  var cartRow = document.createElement("div");
+  cartRow.classList.add("cart-row");
+  var cartItems = document.getElementsByClassName("cart-items")[0];
+  var cartItemNames = cartItems.getElementsByClassName("cart-item-title");
+  for (var i = 0; i < cartItemNames.length; i++) {
+    if (cartItemNames[i].innerText == title) {
+      alert("This item is already added to the cart!");
+      return;
     }
-    document.getElementsByClassName('cart-total-price')[0].innerText = '$' + total;
+  }
+  var cartItems = document.getElementsByClassName("cart-items")[0];
 
+  var cartRowContents = `
+    <div class="cart-item cart-column">
+      <img
+        class="cart-item-image"
+        src="${imageSrc}"
+        width="100"
+        height="100"
+      />
+      <span class="cart-item-title">${title}</span>
+    </div>
 
+    <span class="cart-price cart-column">${price}</span>
+    <div class="cart-quantity cart-column">
+      <input class="cart-quantity-input" type="number" value="1" />
+      <button class="btnn btn-danger" role="button">REMOVE</button>
+    </div>`;
+
+  cartRow.innerHTML = cartRowContents;
+
+  cartItems.append(cartRow);
+
+  cartRow
+    .getElementsByClassName("btn-danger")[0]
+    .addEventListener("click", removeCartItem);
+  cartRow
+    .getElementsByClassName("cart-quantity-input")[0]
+    .addEventListener("change", quantityChanged);
+}
+
+function updateCartTotal() {
+  var cartItemContainer = document.getElementsByClassName("cart-items")[0];
+  var cartRows = cartItemContainer.getElementsByClassName("cart-row");
+  var total = 0;
+  for (var i = 0; i < cartRows.length; i++) {
+    var cartRow = cartRows[i];
+    var priceElement = cartRow.getElementsByClassName("cart-price")[0];
+    var quantityElement = cartRow.getElementsByClassName(
+      "cart-quantity-input"
+    )[0];
+
+    var price = parseFloat(priceElement.innerText.replace("$", ""));
+    //innerText retine valoarea afisata in html pentru pret
+    //parseFloat transf un string intr-un float, adica nr
+    var quantity = quantityElement.value;
+
+    total = total + price * quantity;
+  }
+  total = Math.round(total * 100) / 100;
+  document.getElementsByClassName("cart-total-price")[0].innerText =
+    "$" + total;
 }
